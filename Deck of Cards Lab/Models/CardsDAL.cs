79f5aa327cb542a30e18fc16b7f246750f863a78
Chris Paul <cpaul0013@gmail.com>
2021-11-02 20:52:46 -0400

    Commit

---
+++ Deck of Cards Lab/Models/CardsDAL.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Deck_of_Cards_Lab.Models
+{
+    public class CardsDAL
+    {
+        public CardsModel GetData()
+        {
+            // bad practice but just for today
+
+            // urls in C# need https://
+            string site = "https://deckofcardsapi.com/api/deck/6t2cgu7t1cif/draw/?count=5";
+
+            // request is called request
+            HttpWebRequest request = WebRequest.CreateHttp(site);
+
+            //response is called response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //this will allow it to read the response but won't be readable for humans
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            //This converts the data read in the stream reader into a c# class
+            string JSON = reader.ReadToEnd();
+
+            // takes the string and turns it into an object
+            CardsModel cards = JsonConvert.DeserializeObject<CardsModel>(JSON);
+
+            return cards;
+
+
+
+        }
+
+
+    }
+
+}
